7bb27300851bdabe5302520bc700afe6a9b658b0
Elmer Garduno <garduno@gmail.com>
2010-10-21 21:11:11 +0000

    Completa y valida la factura vs el esquema

--- src/main/java/mx/bigdata/cfdi/Main.java
+++ src/main/java/mx/bigdata/cfdi/Main.java
@@ -19,6 +19,7 @@ package mx.bigdata.cfdi;
 import java.io.File;
 import java.math.BigDecimal;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.*;
@@ -27,7 +28,17 @@ import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Schema;
 
-import mx.bigdata.cfdi.schema.*;
+import mx.bigdata.cfdi.schema.ObjectFactory;
+import mx.bigdata.cfdi.schema.Comprobante;
+import mx.bigdata.cfdi.schema.Comprobante.Conceptos;
+import mx.bigdata.cfdi.schema.Comprobante.Conceptos.Concepto;
+import mx.bigdata.cfdi.schema.Comprobante.Emisor;
+import mx.bigdata.cfdi.schema.Comprobante.Impuestos;
+import mx.bigdata.cfdi.schema.Comprobante.Impuestos.Traslados;
+import mx.bigdata.cfdi.schema.Comprobante.Impuestos.Traslados.Traslado;
+import mx.bigdata.cfdi.schema.Comprobante.Receptor;
+import mx.bigdata.cfdi.schema.TUbicacionFiscal;
+import mx.bigdata.cfdi.schema.TUbicacion;
 
 public final class Main {
 
@@ -48,15 +59,98 @@ public final class Main {
     comp.setVersion("3.0");
     DatatypeFactory dtf = DatatypeFactory.newInstance(); 
     XMLGregorianCalendar cal = dtf
-      .newXMLGregorianCalendar(new GregorianCalendar());
+      .newXMLGregorianCalendar(new GregorianCalendar(2010, 03, 06, 20, 38, 12));
     comp.setFecha(cal);
     comp.setSello("");
     comp.setFormaDePago("PAGO EN UNA SOLA EXHIBICION");
     comp.setNoCertificado("30001000000100000800");
     comp.setCertificado("");
-    comp.setSubTotal(new BigDecimal("13.25"));
-    comp.setTotal(new BigDecimal("14.1"));
+    comp.setSubTotal(new BigDecimal("488.50"));
+    comp.setTotal(new BigDecimal("488.50"));
+    comp.setTipoDeComprobante("ingreso");
+    comp.setEmisor(createEmisor(of));
+    comp.setReceptor(createReceptor(of));
+    comp.setConceptos(createConceptos(of));
+    comp.setImpuestos(createImpuestos(of));
     return comp;
+  }
+    
+  private static Emisor createEmisor(ObjectFactory of) {
+    Emisor emisor = of.createComprobanteEmisor();
+    emisor.setNombre("PHARMA PLUS SA DE CV");
+    emisor.setRfc("PPL961114GZ1");
+    TUbicacionFiscal uf = of.createTUbicacionFiscal();
+    uf.setCalle("AV. RIO MIXCOAC");
+    uf.setCodigoPostal("03240");
+    uf.setColonia("ACACIAS"); 
+    uf.setEstado("MEXICO, D.F."); 
+    uf.setMunicipio("BENITO JUAREZ"); 
+    uf.setNoExterior("No. 140"); 
+    uf.setPais("Mexico"); 
+    emisor.setDomicilioFiscal(uf);
+    return emisor;
+  }
+
+  private static Receptor createReceptor(ObjectFactory of) {
+    Receptor receptor = of.createComprobanteReceptor();
+    receptor.setNombre("JUAN PEREZ PEREZ");
+    receptor.setRfc("PEPJ8001019Q8");
+    TUbicacion uf = of.createTUbicacion();
+    uf.setCalle("AV UNIVERSIDAD");
+    uf.setCodigoPostal("04360");
+    uf.setColonia("COPILCO UNIVERSIDAD"); 
+    uf.setEstado("DISTRITO FEDERAL"); 
+    uf.setMunicipio("COYOACAN"); 
+    uf.setNoExterior("16 EDF 3"); 
+    uf.setNoInterior("DPTO 101"); 
+    uf.setPais("Mexico"); 
+    receptor.setDomicilio(uf);
+    return receptor;
+  }
+
+  private static Conceptos createConceptos(ObjectFactory of) {
+    Conceptos cps = of.createComprobanteConceptos();
+    List<Concepto> list = cps.getConcepto(); 
+    Concepto c1 = of.createComprobanteConceptosConcepto();
+    c1.setUnidad("CAPSULAS");
+    c1.setImporte(new BigDecimal("244.00"));
+    c1.setCantidad(new BigDecimal("1.0"));
+    c1.setDescripcion("VIBRAMICINA 100MG 10");
+    c1.setValorUnitario(new BigDecimal("244.00"));
+    list.add(c1);
+    Concepto c2 = of.createComprobanteConceptosConcepto();
+    c2.setUnidad("BOTELLA");
+    c2.setImporte(new BigDecimal("137.93"));
+    c2.setCantidad(new BigDecimal("1.0"));
+    c2.setDescripcion("CLORUTO 500M");
+    c2.setValorUnitario(new BigDecimal("137.93"));
+    list.add(c2);
+    Concepto c3 = of.createComprobanteConceptosConcepto();    
+    c3.setUnidad("TABLETAS");
+    c3.setImporte(new BigDecimal("84.50"));
+    c3.setCantidad(new BigDecimal("1.0"));
+    c3.setDescripcion("SEDEPRON 250MG 10");
+    c3.setValorUnitario(new BigDecimal("84.50"));
+    list.add(c3);
+    return cps;
+  }
+
+  private static Impuestos createImpuestos(ObjectFactory of) {
+    Impuestos imps = of.createComprobanteImpuestos();
+    Traslados trs = of.createComprobanteImpuestosTraslados();
+    List<Traslado> list = trs.getTraslado(); 
+    Traslado t1 = of.createComprobanteImpuestosTrasladosTraslado();
+    t1.setImporte(new BigDecimal("0.00"));
+    t1.setImpuesto("IVA");
+    t1.setTasa(new BigDecimal("0.0"));
+    list.add(t1);
+    Traslado t2 = of.createComprobanteImpuestosTrasladosTraslado();
+    t2.setImporte(new BigDecimal("22.07"));
+    t2.setImpuesto("IVA");
+    t2.setTasa(new BigDecimal("16.00"));
+    list.add(t2);
+    imps.setTraslados(trs);
+    return imps;
   }
 
 }

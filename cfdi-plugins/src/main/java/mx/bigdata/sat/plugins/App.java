aa5400032a3bf5c750fa111428e17167d4f4e700
Elmer Garduno <garduno@gmail.com>
2010-12-06 05:42:27 +0000

    Cambios al layout de los directorios para soportar milti-modulos

---
+++ cfdi-plugins/src/main/java/mx/bigdata/sat/plugins/App.java
@@ -0,0 +1,13 @@
+package mx.bigdata.sat.plugins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

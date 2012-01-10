9656a18a3bf81a93e9b4e69b54b8391236e61396
Elmer Garduno <garduno@gmail.com>
2012-01-10 04:13:31 +0000

    Se agregaron factories para determinar la version de los archivos

---
+++ cfdi-base/src/main/java/mx/bigdata/sat/cfd/CFD2Factory.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2010 BigData.mx
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package mx.bigdata.sat.cfd;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import mx.bigdata.sat.common.CFDFactory;
+
+import com.google.common.io.ByteStreams;
+
+public final class CFD2Factory extends CFDFactory {
+  
+  public static CFD2 load(File file) throws Exception {
+    InputStream in = new FileInputStream(file);
+    try {
+      return load(in);
+    } finally {
+      in.close();
+    }
+  }
+  
+  public static CFD2 load(InputStream in) throws Exception {
+    return getCFD2(in);
+  }
+  
+  private static CFD2 getCFD2(InputStream in) throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ByteStreams.copy(in, baos);
+    byte[] data = baos.toByteArray();
+    if (getVersion(data).equals("2.2")) {
+      return new CFDv22(new ByteArrayInputStream(data));
+    } else {
+      return new CFDv2(new ByteArrayInputStream(data));
+    }
+  }
+}
---
+++ cfdi-base/src/main/java/mx/bigdata/sat/common/CFDFactory.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2012 BigData.mx
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package mx.bigdata.sat.common;
+
+import java.io.ByteArrayInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+
+public abstract class CFDFactory {
+  
+  protected static String getVersion(byte[] data) throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    Document doc = builder.parse(new ByteArrayInputStream(data));
+    XPathFactory xfactory = XPathFactory.newInstance();
+    XPath xpath = xfactory.newXPath();
+    return (String) xpath.evaluate("/Comprobante/@version", doc);
+  }
+}
